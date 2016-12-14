363af5733b7e3c401a1b6ef950a5b5bf6853b929
Eric Sherman <ericandrewsherman@gmail.com>
2016-12-14 15:25:12 -0600

    init
    export

---
+++ data/json_Top10Schools5.js
@@ -0,0 +1,16 @@
+var json_Top10Schools5={
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "fid": "1", "schoolname": "Fairfield Elementary", "score": "0.15000000000000002", "num_fixtures": "4.0", "lat": "41.780904", "long": "-87.692115", "score_real": 0.15, "rank": 8.0 }, "geometry": { "type": "Point", "coordinates": [ -87.692115, 41.780904 ] } },
+{ "type": "Feature", "properties": { "fid": "2", "schoolname": "Robeson High School", "score": "0.14666666666666667", "num_fixtures": "15.0", "lat": "41.769714", "long": "-87.637153", "score_real": 0.14666666666666667, "rank": 9.0 }, "geometry": { "type": "Point", "coordinates": [ -87.637153, 41.769714 ] } },
+{ "type": "Feature", "properties": { "fid": "3", "schoolname": "Onahan", "score": "0.15454545454545454", "num_fixtures": "22.0", "lat": "41.99379", "long": "-87.79310799999998", "score_real": 0.15454545454545454, "rank": 7.0 }, "geometry": { "type": "Point", "coordinates": [ -87.793108, 41.99379 ] } },
+{ "type": "Feature", "properties": { "fid": "4", "schoolname": "Wentworth Elementary", "score": "0.175", "num_fixtures": "16.0", "lat": "41.765076", "long": "-87.65820500000002", "score_real": 0.175, "rank": 5.0 }, "geometry": { "type": "Point", "coordinates": [ -87.658205, 41.765076 ] } },
+{ "type": "Feature", "properties": { "fid": "5", "schoolname": "Tanner Elementary", "score": "0.21875", "num_fixtures": "16.0", "lat": "41.760673", "long": "-87.606791", "score_real": 0.21875, "rank": 3.0 }, "geometry": { "type": "Point", "coordinates": [ -87.606791, 41.760673 ] } },
+{ "type": "Feature", "properties": { "fid": "6", "schoolname": "Orr Hs", "score": "0.1602941176470588", "num_fixtures": "34.0", "lat": "41.894449", "long": "-87.726166", "score_real": 0.16029411764705881, "rank": 6.0 }, "geometry": { "type": "Point", "coordinates": [ -87.726166, 41.894449 ] } },
+{ "type": "Feature", "properties": { "fid": "7", "schoolname": "Field", "score": "0.2076923076923077", "num_fixtures": "13.0", "lat": "42.009664", "long": "-87.669985", "score_real": 0.2076923076923077, "rank": 4.0 }, "geometry": { "type": "Point", "coordinates": [ -87.669985, 42.009664 ] } },
+{ "type": "Feature", "properties": { "fid": "8", "schoolname": "Salazar", "score": "0.25", "num_fixtures": "4.0", "lat": "41.901417", "long": "-87.63363199999998", "score_real": 0.25, "rank": 2.0 }, "geometry": { "type": "Point", "coordinates": [ -87.633632, 41.901417 ] } },
+{ "type": "Feature", "properties": { "fid": "9", "schoolname": "Reilly Elementary", "score": "0.25999999999999995", "num_fixtures": "10.0", "lat": "41.941094", "long": "-87.719541", "score_real": 0.26, "rank": 1.0 }, "geometry": { "type": "Point", "coordinates": [ -87.719541, 41.941094 ] } },
+{ "type": "Feature", "properties": { "fid": "10", "schoolname": "Blair", "score": "0.14400000000000002", "num_fixtures": "25.0", "lat": "41.776556", "long": "-87.790604", "score_real": 0.144, "rank": 10.0 }, "geometry": { "type": "Point", "coordinates": [ -87.790604, 41.776556 ] } }
+]
+}
